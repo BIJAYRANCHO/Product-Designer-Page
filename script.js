d65edb60820a6426b88a7e6b6b81ac3341b398d1
BIJAY GHOSH <bijayghosh.email@gmail.com>
2023-08-02 13:53:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// SMOOTH SCROLLING
+/*
+    attach loco scroll css
+    attach locomotive scroll min js
+    some code from loco github for js 
+*/
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
+// GSAP
+// SCROLL TRIGGER
